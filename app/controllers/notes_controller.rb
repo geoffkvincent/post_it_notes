9789deb42585bcd97cee866e9e29b8fd35d84910
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-12 12:18:57 -0700

    controller basic setup

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,24 @@
 class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
   
 end
